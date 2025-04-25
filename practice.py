787c3f6740064149789816eb3764c4b6b3bed7b3
TheePoseidon <j.ngugigiat@alustudent.com>
2025-04-25 12:25:36 +0300

    practice

--- practice.py
+++ practice.py
@@ -1,4 +1,7 @@
-languages = ['python', 'swift', 'C++']
+#languages = ['python', 'swift', 'C++']
+#
+#for language in languages:
+#    print (language)
 
-for language in languages:
-    print (language)
+numbers = [number*number for number in range (1, 10)]
+print (numbers)
