7ab1622f4175820ccc1e4fc8bc676e20065a865e
TheePoseidon <j.ngugigiat@alustudent.com>
2025-04-25 12:55:44 +0300

    practice

--- tuple.py
+++ tuple.py
@@ -1,3 +1,7 @@
-number1 = ('Python', 2, 34.5)
-
-print(type(number1[1]))
+# number1 = ('Python', 2, 34.5)
+#
+# print(type(number1[1]))
+#
+# letters = ('j', 'a,', 'm', 'e', 's')
+# print (letters[2])
+# print (letters[0:3])
